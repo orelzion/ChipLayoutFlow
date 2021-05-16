91775d6738600ec968dff5f45304122b30e798ea
orelzion <orel@viz.ai>
2021-05-16 10:04:02 +0300

    added regex

--- app/src/main/java/com/github/orelzion/flowchiplayout/MainActivity.kt
+++ app/src/main/java/com/github/orelzion/flowchiplayout/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
@@ -13,6 +14,10 @@ import com.github.orelzion.flowchiplayout.databinding.ActivityMainBinding
 import com.google.android.material.chip.Chip
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val EMAIL_REGEX = "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +36,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun afterTextChanged(viewBinding: ActivityMainBinding, text: String) {
         if (text.length > 1 && text.endsWith(",")) {
-            val chip = createChip(withText = text.substringBeforeLast(","))
+            val emailInput = text.substringBeforeLast(",")
+
+            if(!emailInput.matches(EMAIL_REGEX.toRegex())) {
+                Toast.makeText(this, R.string.email_input_error, Toast.LENGTH_SHORT).show()
+                viewBinding.emailInput.text = null
+                return
+            }
+
+            val chip = createChip(withText = emailInput)
             viewBinding.root.addView(chip)
 
             val updatedArray = viewBinding.mailsLayout.referencedIds.run {
